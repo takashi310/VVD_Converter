22e7805ecdc34c8871bddd1efcc50a2547a90b28
takashi310 <kawase@gr.bot.kyoto-u.ac.jp>
2017-09-25 13:20:24 -0400

    ZLIB compression and multi-threading

--- vvd_export.java
+++ vvd_export.java
@@ -10,6 +10,8 @@ import ij.plugin.frame.*;
 import java.io.*;
 import java.nio.*;
 import java.util.*;
+import java.util.zip.*;
+import java.util.concurrent.atomic.AtomicInteger;
 
 import javax.xml.parsers.*;
 import javax.xml.transform.*;
@@ -31,6 +33,8 @@ public class vvd_export implements PlugIn {
 	ArrayList<Integer> bwlist = new ArrayList<Integer>();
 	ArrayList<Integer> bhlist = new ArrayList<Integer>();
 	ArrayList<Integer> bdlist = new ArrayList<Integer>();
+	int thread_num_ = (int)Prefs.get("thread_num.int",4);
+
 
 	class Brick {
 		public int x_, y_, z_;
@@ -64,13 +68,14 @@ public class vvd_export implements PlugIn {
 	}
 	
 	private boolean showDialog() {
-		String[] types = {"RAW", "JPEG"};
+		String[] types = {"RAW", "JPEG", "ZLIB"};
 		GenericDialog gd = new GenericDialog("Generate Bricks");
-		gd.addNumericField("Brick width  (default)",  bw, 0);
-		gd.addNumericField("Brick height (default)", bh, 0);
-		gd.addNumericField("Brick depth  (default)",  bd, 0);
+		gd.addNumericField("BrickWidth  (default)",  bw, 0);
+		gd.addNumericField("BrickHeight (default)", bh, 0);
+		gd.addNumericField("BrickDepth  (default)",  bd, 0);
 		gd.addNumericField("Levels", lv, 0);
 		gd.addChoice("FileType", types, types[0]);
+		gd.addNumericField("Thread", thread_num_, 0);
 		gd.showDialog();
 		if (gd.wasCanceled()) return false;
 		bw = (int)gd.getNextNumber();
@@ -78,6 +83,10 @@ public class vvd_export implements PlugIn {
 		bd = (int)gd.getNextNumber();
 		lv = (int)gd.getNextNumber();
 		filetype = types[gd.getNextChoiceIndex()];
+		thread_num_ = (int)gd.getNextNumber();
+		if (thread_num_ <= 0) thread_num_ = 1;
+		
+		Prefs.set("thread_num.int",thread_num_);
 		return true;
 	}
 
@@ -106,9 +115,9 @@ public class vvd_export implements PlugIn {
 		for(int i = 0; i < lv; i++) { 
 			if(i == 0) gd.setInsets(0,0,0);
 			if(i > 0) gd.setInsets(5,0,0);
-			gd.addMessage("Level: " + String.valueOf(i), font);
-			gd.addChoice("Image: ", titles, i < tnum ? titles[i] : titles[tnum] );
-			gd.addStringField("Brick size (w h d)",  bw + " " + bh + " " + bd);
+			gd.addMessage("Level:" + String.valueOf(i), font);
+			gd.addChoice("Image"+ String.valueOf(i)+":", titles, i < tnum ? titles[i] : titles[tnum] );
+			gd.addStringField("Bricksize"+ String.valueOf(i)+" (w h d)",  bw + " " + bh + " " + bd);
 		}
 		gd.showDialog();
 		if (gd.wasCanceled()) return false;
@@ -150,7 +159,7 @@ public class vvd_export implements PlugIn {
 		ImagePlus orgimp;
 		ImageStack stack;
 		FileInfo finfo;
-
+				
 		if(lvImgTitle.isEmpty()) return;
 		orgimp = WindowManager.getImage(lvImgTitle.get(0));
 		imp = orgimp;
@@ -195,7 +204,6 @@ public class vvd_export implements PlugIn {
 		doc.appendChild(root);
 
 		for(int l = 0; l < lv; l++){
-			IJ.showProgress(0.0);
 			
 			int[] dims2 = imp.getDimensions();
 			IJ.log("W: " + String.valueOf(dims2[0]) +
@@ -245,11 +253,13 @@ public class vvd_export implements PlugIn {
 			brksnode.setAttribute("brick_baseD", String.valueOf(bd));
 			lvnode.appendChild(brksnode);
 
-			ArrayList<Brick> bricks = new ArrayList<Brick>();
+			final ArrayList<Brick> bricks = new ArrayList<Brick>();
 			int mw, mh, md, mw2, mh2, md2;
 			double tx0, ty0, tz0, tx1, ty1, tz1;
 			double bx0, by0, bz0, bx1, by1, bz1;
+			ArrayList<Integer> xy_brk_count = new ArrayList<Integer>();
 			for(int k = 0; k < imageD; k += bd){
+				int xy_count = 0;
 				if(k > 0) k--;
 				for(int j = 0; j < imageH; j += bh){
 					if(j > 0) j--;
@@ -309,8 +319,10 @@ public class vvd_export implements PlugIn {
 						y = j - (mh2 - mh);
 						z = k - (md2 - md);
 						bricks.add(new Brick(x, y, z, mw2, mh2, md2, 0, 0, tx0, ty0, tz0, tx1, ty1, tz1, bx0, by0, bz0, bx1, by1, bz1));
+						xy_count++;
 					}
 				}
+				xy_brk_count.add(xy_count);
 			}
 
 			Element fsnode = doc.createElement("Files");
@@ -322,145 +334,206 @@ public class vvd_export implements PlugIn {
 			int curbricknum = 0;
 			for(int f = 0; f < nFrame; f++){
 				for(int ch = 0; ch < nCh; ch++){
-				
-					Brick b_first = bricks.get(0);
-					if(b_first.z_ != 0) IJ.log("warning");
-					int st_z = b_first.z_;
-					int ed_z = b_first.z_ + b_first.d_;
+					int first_bid = 0;
+					int st_z = 0;
+					int ed_z = 0;
 					LinkedList<ImageProcessor> iplist = new LinkedList<ImageProcessor>();
-					for(int s = st_z; s < ed_z; s++)
-						iplist.add( stack.getProcessor(imp.getStackIndex(ch+1, s+1, f+1)) );
+					for (int ll = 0; ll < xy_brk_count.size(); ll++) {
+						Brick b_first = bricks.get(first_bid);
 
-//					ImagePlus test;
-//					ImageStack tsst;
-//					test = NewImage.createByteImage("test", imageW, imageH, imageD, NewImage.FILL_BLACK);
-//					tsst = test.getStack();
-					for(int i = 0; i < bricks.size(); i++){
-						Brick b = bricks.get(i);
-
-						if(ed_z > b.z_ || st_z < b.z_+b.d_){
-							if(b.z_ > st_z){
-								for(int s = 0; s < b.z_-st_z; s++)iplist.pollFirst();
-								st_z = b.z_;
+						if (ll == 0) {
+							st_z = b_first.z_;
+							ed_z = b_first.z_ + b_first.d_;
+							for(int s = st_z; s < ed_z; s++)
+								iplist.add( stack.getProcessor(imp.getStackIndex(ch+1, s+1, f+1)) );
+						}
+						else if(ed_z > b_first.z_ || st_z < b_first.z_+b_first.d_){
+							if(b_first.z_ > st_z){
+								for(int s = 0; s < b_first.z_-st_z; s++)iplist.pollFirst();
+								st_z = b_first.z_;
 							}
-							else if(b.z_ < st_z){
+							else if(b_first.z_ < st_z){
 								IJ.log("warning");
-								for(int s = st_z-1; s > b.z_; s--)
+								for(int s = st_z-1; s > b_first.z_; s--)
 									iplist.addFirst( stack.getProcessor(imp.getStackIndex(ch+1, s+1, f+1)) );
-								st_z = b.z_;
+								st_z = b_first.z_;
 							}
 							
-							if(b.z_+b.d_ > ed_z){
-								for(int s = ed_z; s < b.z_+b.d_; s++)
+							if(b_first.z_+b_first.d_ > ed_z){
+								for(int s = ed_z; s < b_first.z_+b_first.d_; s++)
 									iplist.add( stack.getProcessor(imp.getStackIndex(ch+1, s+1, f+1)) );
-									ed_z = b.z_+b.d_;
+									ed_z = b_first.z_+b_first.d_;
 							}
-							else if(b.z_+b.d_ < ed_z){
+							else if(b_first.z_+b_first.d_ < ed_z){
 								IJ.log("warning");
-								for(int s = 0; s < ed_z-(b.z_+b.d_); s++)iplist.pollLast();
-								ed_z = b.z_+b.d_;
+								for(int s = 0; s < ed_z-(b_first.z_+b_first.d_); s++)iplist.pollLast();
+								ed_z = b_first.z_+b_first.d_;
 							}
 						}
 						else {
 							IJ.log("warning");
 							iplist.clear();
-							st_z = b.z_;
-							ed_z = b.z_+b.d_;
+							st_z = b_first.z_;
+							ed_z = b_first.z_+b_first.d_;
 							for(int s = st_z; s < ed_z; s++)
 							iplist.add( stack.getProcessor(imp.getStackIndex(ch+1, s+1, f+1)) );
 						}
 
-						if(iplist.size() != b.d_){
-							IJ.log("Stack Error");
+						if(iplist.size() != b_first.d_){
+							IJ.log("Stack Error "+iplist.size()+" "+b_first.d_);
 							return;
 						}
 
-//						int zz = st_z;
+						final int st_bid = first_bid;
+						final int ed_bid = first_bid + xy_brk_count.get(ll) - 1;
 
-						int bsize = 0;
-						byte[] bdata = new byte[b.w_*b.h_*b.d_*bdepth/8];
+						final AtomicInteger ai = new AtomicInteger(st_bid);
+						final Thread[] threads = newThreadArray();
+
 						Iterator<ImageProcessor> ipite = iplist.iterator();
+						final ArrayList<ImageProcessor> iplist_final = new ArrayList<ImageProcessor>();
 						while (ipite.hasNext()){
-
-//							ImageProcessor tsip = tsst.getProcessor(zz+1);
-							
-							ImageProcessor ip = ipite.next();
-							ip.setRoi(b.x_, b.y_, b.w_, b.h_);
-							if(bdepth == 8){
-								byte[] data = (byte[])ip.crop().getPixels();
-								System.arraycopy(data, 0, bdata, bsize, data.length);
-								bsize += data.length;
-//								byte[] data = (byte[])ip.crop().getPixels();
-//								for(int yy = 0; yy < b.h_; yy++){
-//									for(int xx = 0; xx < b.w_; xx++){
-//										tsip.putPixel(xx+b.x_, yy+b.y_, data[yy*b.w_+xx]);
-//									}
-//								}
-//								zz++;
-							}
-							else if(bdepth == 16){
-								ByteBuffer buffer = ByteBuffer.allocate(b.w_*b.h_*bdepth/8);
-								buffer.order(ByteOrder.LITTLE_ENDIAN);
-								short[] data = (short[])ip.crop().getPixels();
-								for(short e : data)buffer.putShort(e);
-								System.arraycopy(buffer.array(), 0, bdata, bsize, buffer.array().length);
-								bsize += buffer.array().length;
-							}else if(bdepth == 32){
-								ByteBuffer buffer = ByteBuffer.allocate(b.w_*b.h_*bdepth/8);
-								buffer.order(ByteOrder.LITTLE_ENDIAN);
-								float[] data = (float[])ip.crop().getPixels();
-								for(float e : data)buffer.putFloat(e);
-								System.arraycopy(buffer.array(), 0, bdata, bsize, buffer.array().length);
-								bsize += buffer.array().length;
-							}
+							iplist_final.add(ipite.next());
 						}
-						
-						String filename = basename + "_Lv" + String.valueOf(l) +
-													 "_Ch" + String.valueOf(ch) +
-													 "_Fr" + String.valueOf(f) +
-													 "_ID" + String.valueOf(i);
-					
-						if (filetype == "RAW") {
-							filename += ".raw";
-							BufferedOutputStream fis = null;
-							try {
-								File file = new File(directory + filename);
-								fis = new BufferedOutputStream(new FileOutputStream(file));
-								fis.write(bdata);
+
+						final int pitchY = imageW;
+						final String fftype = filetype;
+						final String fbasename = basename;
+						final ArrayList<Element> elems = new ArrayList<Element>();
+						for (int e = 0; e < xy_brk_count.get(ll); e++)
+							elems.add(doc.createElement("File"));
+
+						final int fbd = bdepth;
+						final int flv = l;
+						final int fch = ch;
+						final int ffr = f;
+		
+						for (int ithread = 0; ithread < threads.length; ithread++) {
+							// Concurrently run in as many threads as CPUs
+							threads[ithread] = new Thread() {
 								
-							} catch (IOException e) {
-								e.printStackTrace();
-								return;
-							} finally {
-								try {
-									if (fis != null) fis.close();
-								} catch (IOException e) {
-									e.printStackTrace();
-									return;
-								}
-							}
-						}
-						if (filetype == "JPEG" && bdepth == 8) {
-							filename += ".jpg";
-							ByteProcessor bip = new ByteProcessor(b.w_, b.h_*b.d_, bdata);
-							FileSaver fs = new FileSaver(new ImagePlus("", bip));
-							fs.setJpegQuality(jpeg_quality);
-							fs.saveAsJpeg(directory + filename);
-						}
+								{ setPriority(Thread.NORM_PRIORITY); }
+				
+								public void run() {
+					
+									for (int i = ai.getAndIncrement(); i <= ed_bid; i = ai.getAndIncrement()) {
+										Brick b = bricks.get(i);
+										int bsize = 0;
+										byte[] bdata = new byte[b.w_*b.h_*b.d_*fbd/8];
 
-						Element filenode = doc.createElement("File");
-						filenode.setAttribute("filename", filename);
-						filenode.setAttribute("channel", String.valueOf(ch));
-						filenode.setAttribute("frame", String.valueOf(f));
-						filenode.setAttribute("brickID", String.valueOf(i));
-						fsnode.appendChild(filenode);
-	
-						curbricknum++;
-						IJ.showProgress((double)(curbricknum)/(double)(totalbricknum));
+										for (int p = 0; p < iplist_final.size(); p++){
+											ImageProcessor ip = iplist_final.get(p);
+											ip.setRoi(b.x_, b.y_, b.w_, b.h_);
+											if(fbd == 8){
+												byte[] data = (byte[])ip.getPixels();
+												for (int yy = b.y_; yy < b.y_+b.h_; yy++) {
+													int srcpos = yy*pitchY + b.x_;
+													System.arraycopy(data, srcpos, bdata, bsize, b.w_);
+													bsize += b.w_;
+												}
+												data = null;
+											} else if (fbd == 16) {
+												ByteBuffer buffer = ByteBuffer.allocate(b.w_*b.h_*fbd/8);
+												buffer.order(ByteOrder.LITTLE_ENDIAN);
+												short[] data = (short[])ip.getPixels();
+												for (int yy = b.y_; yy < b.y_+b.h_; yy++) {
+													for (int xx = b.x_; xx < b.x_+b.w_; xx++) {
+														buffer.putShort((short)(data[yy*pitchY+xx]<<4));
+													}
+												}
+												System.arraycopy(buffer.array(), 0, bdata, bsize, buffer.array().length);
+												bsize += buffer.array().length;
+												data = null;
+												buffer = null;
+											} else if (fbd == 32) {
+												ByteBuffer buffer = ByteBuffer.allocate(b.w_*b.h_*fbd/8);
+												buffer.order(ByteOrder.LITTLE_ENDIAN);
+												float[] data = (float[])ip.getPixels();
+												for (int yy = b.y_; yy < b.y_+b.h_; yy++) {
+													for (int xx = b.x_; xx < b.x_+b.w_; xx++) {
+														buffer.putFloat(data[yy*pitchY+xx]);
+													}
+												}
+												System.arraycopy(buffer.array(), 0, bdata, bsize, buffer.array().length);
+												bsize += buffer.array().length;
+												data = null;
+												buffer = null;
+											}
+										}
 
+										String filename = fbasename + "_Lv" + String.valueOf(flv) +
+																	  "_Ch" + String.valueOf(fch) +
+																	  "_Fr" + String.valueOf(ffr) +
+																	  "_ID" + String.valueOf(i);
+					
+										if (fftype == "RAW") {
+											filename += ".raw";
+											BufferedOutputStream fis = null;
+											try {
+												File file = new File(directory + filename);
+												fis = new BufferedOutputStream(new FileOutputStream(file));
+												fis.write(bdata);
+												fis.close();
+												fis = null;
+											} catch (IOException e) {
+												e.printStackTrace();
+												return;
+											}
+										}
+										if (fftype == "JPEG" && fbd == 8) {
+											filename += ".jpg";
+											ByteProcessor bip = new ByteProcessor(b.w_, b.h_*b.d_, bdata);
+											FileSaver fs = new FileSaver(new ImagePlus("", bip));
+											fs.setJpegQuality(jpeg_quality);
+											fs.saveAsJpeg(directory + filename);
+											fs = null;
+											bip = null;
+										}
+										if (fftype == "ZLIB") {
+											byte[] tmpdata = new byte[b.w_*b.h_*b.d_*fbd/8];
+											Deflater compresser = new Deflater(Deflater.BEST_SPEED);
+											compresser.setInput(bdata);
+											compresser.finish();
+											int datasize = compresser.deflate(tmpdata);
+											//IJ.log(""+datasize);
+											compresser.end();
+							
+											filename += ".zlib";
+											BufferedOutputStream fis = null;
+											try {
+												File file = new File(directory + filename);
+												fis = new BufferedOutputStream(new FileOutputStream(file));
+												fis.write(tmpdata, 0, datasize);
+												fis.close();
+											} catch (IOException e) {
+												e.printStackTrace();
+												return;
+											}
+											tmpdata = null;
+										}//if (filetype == "ZLIB") {
+
+										Element filenode = elems.get(i - st_bid);
+										filenode.setAttribute("filename", filename);
+										filenode.setAttribute("channel", String.valueOf(fch));
+										filenode.setAttribute("frame", String.valueOf(ffr));
+										filenode.setAttribute("brickID", String.valueOf(i));
+										filenode.setAttribute("filetype", String.valueOf(fftype));
+											
+										bdata = null;
+
+									}//	for (int i = ai.getAndIncrement(); i < names.length;
+							}};//threads[ithread] = new Thread() {
+						}//	for (int ithread = 0; ithread < threads.length; ithread++) {
+						
+						startAndJoin(threads);
+						for (Element e : elems) fsnode.appendChild(e);
+						first_bid += xy_brk_count.get(ll);
 					}
-				}
-			}
+					iplist.clear();
+					System.gc();
+
+				}//for(int ch = 0; ch < nCh; ch++)
+			}//for(int f = 0; f < nFrame; f++)
 
 			for(int i = 0; i < bricks.size(); i++){
 				Brick b = bricks.get(i);
@@ -507,11 +580,17 @@ public class vvd_export implements PlugIn {
 			}
 			
 		}
-		
-		File newXMLfile = new File(directory + basename + ".vvd");
-		writeXML(newXMLfile, doc);
 
-		imp.show();
+		try{
+			File newXMLfile = new File(directory + basename + ".vvd");
+			newXMLfile.createNewFile();
+			writeXML(newXMLfile, doc);
+		} catch (IOException e) {
+			e.printStackTrace();
+			return;
+		}
+
+	
 	}
 
 
@@ -567,4 +646,38 @@ public class vvd_export implements PlugIn {
 		while (val < dim) val = val << 1;
 		return val;
 	}
+
+	/** Create a Thread[] array as large as the number of processors available.
+		* From Stephan Preibisch's Multithreading.java class. See:
+		* http://repo.or.cz/w/trakem2.git?a=blob;f=mpi/fruitfly/general/MultiThreading.java;hb=HEAD
+		*/
+	private Thread[] newThreadArray() {
+		int n_cpus = Runtime.getRuntime().availableProcessors();
+		if (n_cpus > thread_num_) n_cpus = thread_num_;
+		if (n_cpus <= 0) n_cpus = 1;
+		return new Thread[n_cpus];
+	}
+	
+	/** Start all given threads and wait on each of them until all are done.
+		* From Stephan Preibisch's Multithreading.java class. See:
+		* http://repo.or.cz/w/trakem2.git?a=blob;f=mpi/fruitfly/general/MultiThreading.java;hb=HEAD
+		*/
+	public static void startAndJoin(Thread[] threads)
+	{
+		for (int ithread = 0; ithread < threads.length; ++ithread)
+		{
+			threads[ithread].setPriority(Thread.NORM_PRIORITY);
+			threads[ithread].start();
+		}
+		
+		try
+		{   
+			for (int ithread = 0; ithread < threads.length; ++ithread)
+			threads[ithread].join();
+		} catch (InterruptedException ie)
+		{
+			throw new RuntimeException(ie);
+		}
+	}
+
 }
